f9500d5d397305221ddcb36e27d4c00b92c44073
catliba <1caleblili@gmail.com>
2024-03-30 00:42:27 -0700

    cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,10 @@
-module.exports = () => ({});
+module.exports = {
+    upload: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: 'dub4kzx9e',
+        api_key: '384846467298923',
+        api_secret: 'UI-1J6vrTqBW3QJuXSiYdNEYELU',
+      },
+    },
+  };
